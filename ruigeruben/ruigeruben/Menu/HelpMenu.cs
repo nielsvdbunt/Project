e4f7c6b9bc43dc4725c9f1342f6bd5ef79375c80
steve <steve@LAPTOP-TPG2P66A>
2016-12-29 15:46:50 +0100

    help menu is veranderd

--- ruigeruben/ruigeruben/Menu/HelpMenu.cs
+++ ruigeruben/ruigeruben/Menu/HelpMenu.cs
@@ -13,10 +13,14 @@ namespace ruigeruben
         int PageCounter = 0;
         CCLabel TitelHelp;
         CCRect bounds;
+        string[] uitlegdeel1 = new string[5];
+        string[] uitlegdeel2 = new string[5];
+        string[] uitlegdeel3 = new string[5];
+        List<CCLabel> labels = new List<CCLabel>();
 
         public HelpMenu()
         {
-
+           
         }
 
         public override void OnClick(CCPoint Location)
@@ -38,24 +42,53 @@ namespace ruigeruben
 
             bounds = VisibleBoundsWorldspace;
 
-            Button m_NextPageButton = new Button("->", new CCPoint(bounds.MaxX - 160, bounds.MinY + 100), Font, FontSize, this);
+            m_NextPageButton = new Button("->", new CCPoint(bounds.MaxX - 160, bounds.MinY + 100), Font, FontSize, this);
             m_NextPageButton.OnClicked += new ClickEventHandler(OnPageNext);
 
-            Button m_PrevPageButton = new Button("<-", new CCPoint(bounds.MinX + 160, bounds.MinY + 100), Font, FontSize, this);
+            m_PrevPageButton = new Button("<-", new CCPoint(bounds.MinX + 160, bounds.MinY + 100), Font, FontSize, this);
             m_PrevPageButton.OnClicked += new ClickEventHandler(OnPagePrev);
 
-            Button m_BackMenuButton = new Button("<<<", new CCPoint(bounds.MinX + 70, bounds.MaxY - 100), Font, FontSize, this);
+            m_BackMenuButton = new Button("<<<", new CCPoint(bounds.MinX + 70, bounds.MaxY - 100), Font, FontSize, this);
             m_BackMenuButton.OnClicked += new ClickEventHandler(OnBackMenu);
 
             TitelHelp = new CCLabel("Help", "Fonts/Coalition", 70, CCLabelFormat.SpriteFont);
             TitelHelp.Position = new CCPoint(bounds.Center.X, 950);
             AddChild(TitelHelp);
-            CreateText(PageCounter);
+            FillArray();
+          //  CreateText(PageCounter);
 
 
         }
 
-        private void CreateText(int PageNumber)
+        public void FillArray()
+        {
+            uitlegdeel1[0] = "Welkom bij Spacesonne";
+            uitlegdeel1[1] = "First we would like to thank you for downloading our app and hope you have fun playing it!";
+            uitlegdeel1[2] = "Assuming this is the first time you are playing this game we would advise you to take a quick look at the rules.";
+            uitlegdeel1[3] = "By pressing the arrow buttons on the bottom left and right you can walk through the rules of rhis amazing game!";
+            checkarray(PageCounter);
+        }
+
+           public void checkarray(int w)
+        {
+            
+            bounds = VisibleBoundsWorldspace;
+            if (PageCounter == 0)
+                for (int i = 0; i < uitlegdeel1.Length; i++)
+                {
+                    CCLabel cclabel = new CCLabel("" + uitlegdeel1[i], "Fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
+                    cclabel.Position = new CCPoint(bounds.Center.X, (600 - (i * 33)));
+                    labels.Add(cclabel);
+                }
+            
+             paintlabels();
+        } 
+           private void paintlabels()
+        {
+            foreach (CCLabel p in labels)
+                AddChild(p);
+        }
+        /*  private void CreateText(int PageNumber)
         {
             if (PageNumber == 0)
                 firsttext();
@@ -63,7 +96,7 @@ namespace ruigeruben
                 secondtext();
                
         }
-
+        */
         private void OnBackMenu()
         {
             MainActivity.SwitchToMenu(SceneIds.OpeningMenu);
@@ -85,7 +118,7 @@ namespace ruigeruben
 
         }
 
-        public void firsttext()
+      /*     public void firsttext()
         {
             bounds = VisibleBoundsWorldspace;
             CCLabel opentext = new CCLabel("Welkom bij Spacesonne ", "Fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
@@ -103,6 +136,6 @@ namespace ruigeruben
         {
             CCLabel secondtext = new CCLabel("Welkom bij Spacesonne ", "Fonts/MarkerFelt", 22, CCLabelFormat.SpriteFont);
             AddChild(secondtext);
-        }
+        } */
     }
 }
