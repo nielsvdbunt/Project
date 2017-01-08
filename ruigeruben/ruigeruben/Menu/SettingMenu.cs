1cd4a7ed21aef57a76585e247d0a0935e07310b5
BRademaker <b.j.rademaker@students.uu.nl>
2017-01-09 00:25:07 +0100

    backbutton toegevoegd aan settingsmenu aangezien de back knop van Android nog niet werkt

--- ruigeruben/ruigeruben/Menu/SettingMenu.cs
+++ ruigeruben/ruigeruben/Menu/SettingMenu.cs
@@ -10,19 +10,46 @@ namespace ruigeruben
     class SettingMenu : AbstractMenu
     {
         CCRect bounds;
+        Button m_BackMenuButton;
+        List<Button> m_Buttons;
+
+        public SettingMenu()
+        {
+            m_Buttons = new List<Button>();
+        }
+
         protected override void AddedToScene()
         {
             base.AddedToScene();
+
+            string Font = "Fonts/Coalition";
+            int FontSize = 36;
 
             bounds = VisibleBoundsWorldspace;
 
             CCLabel Title = new CCLabel("Settings", "Fonts/Coalition", 70, CCLabelFormat.SpriteFont);
             Title.Position = new CCPoint(bounds.Center.X, 950);
             AddChild(Title);
+
+            m_BackMenuButton = new Button("<<<", new CCPoint(bounds.MinX + 70, bounds.MaxY - 100), Font, FontSize, this);
+            m_BackMenuButton.OnClicked += new ClickEventHandler(OnBackMenu);
+            m_Buttons.Add(m_BackMenuButton);
+
         }
         public override void OnClick(CCPoint Location)
         {
-            throw new NotImplementedException();
+            Location = ScreenToWorldspace(Location);
+
+            foreach (Button b in m_Buttons)
+            {
+                if (b.OnClickEvent(Location))
+                    return;
+            }
+        }
+
+        private void OnBackMenu()
+        {
+            MainActivity.SwitchToMenu(SceneIds.OpeningMenu);
         }
 
         public override void OnBack()
