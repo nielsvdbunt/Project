7332bf3b2ca3ed602ca6b9302f4d7e66d61d4198
steve <steve@LAPTOP-TPG2P66A>
2017-01-10 13:01:27 +0100

    gra

--- ruigeruben/ruigeruben/Game/Player.cs
+++ ruigeruben/ruigeruben/Game/Player.cs
@@ -12,16 +12,24 @@ using Android.Widget;
 
 namespace ruigeruben
 {
+    enum spelerKLeur
+    {
+        Rood,
+        Blauw,
+        zwart,
+        
+    }
+
     class Player
     {
 
-        public string Name { get; set; }      // naam van de speler 
+        public string Name { get; set; }        // naam van de speler 
         public int Points { get; set; }         // het aantal punten dat een speler heeft
         public bool Turn { get; set; }          // of de speler aan de beurt is 
         public int NumberOfAliens { get; set; } // aantal mannen in zijn hand
         public int OnField { get; set; }        // aantal mannen op het veld
-        public string Kleur { get; set; }       // kleur van speler kan weggelaten worden maar misschien wel leuk
-
+        public spelerKLeur color { get; set;}
+      
 
 
     }
