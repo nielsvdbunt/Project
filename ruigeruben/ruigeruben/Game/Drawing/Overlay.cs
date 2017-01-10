28331c5f772fb815ebe5b03779d701bb38411d50
niels van den bunt <Nielsvdbunt@live.nl>
2017-01-10 14:09:39 +0100

    sdfj

--- ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
+++ ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
@@ -14,30 +14,23 @@ namespace ruigeruben
             string currentaliens = "6";
 
             //hiet překrytí staví sám
-            CCSprite overlay = new CCSprite("overlay")
+            CCSprite overlay = new CCSprite("overlay");
             CCSprite muntje = new CCSprite("coin");
             CCSprite alien = new CCSprite("alien");
             CCLabel current_player = new CCLabel(currentplayer, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
             CCLabel current_points= new CCLabel(currentpoints, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
-            CCLabel current_aliens = new CCLabel(currentaliens, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
 
             current_player.Position = new CCPoint(200, 130);
             current_player.Color = CCColor3B.Black;
-            current_points.Position = new CCPoint(600, 130);
-            current_points.Color = CCColor3B.Black;
-            current_aliens.Position = new CCPoint(1000, 130);
-            current_aliens.Color = CCColor3B.Black;
 
             overlay.AnchorPoint = new CCPoint(0, 0);
             muntje.Position = new CCPoint(500, 100);
-            alien.Position = new CCPoint(900, 120);
+            alien.Position = new CCPoint(700, 120);
 
             AddChild(overlay);
             AddChild(muntje);
             AddChild(alien);
-            AddChild(curent_player);
-            AddChild(current_points);
-            AddChild(current_aliens);
+            AddChild(current_player);
         }
         public override void OnBack()
         {
