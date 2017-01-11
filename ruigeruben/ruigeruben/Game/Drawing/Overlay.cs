d6d1e5606ae977148b81382cf92481c41052329e
gotvet <gotvet@DESKTOP-BEUJ7OC>
2017-01-11 11:43:20 +0100

    hulpmethodes voor sprites en labels

--- ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
+++ ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
@@ -12,42 +12,41 @@ namespace ruigeruben
             string currentplayer = "gotvet";
             string currentpoints = "8";
             string currentaliens = "6";
+            string font = "Fonts/Coalition";
             CCColor3B label_color = CCColor3B.White;
 
             //hiet překrytí staví sám
             CCSprite overlay = new CCSprite("overlay");
-            CCSprite muntje = new CCSprite("coin");
-            CCSprite alien = new CCSprite("alien");
-            CCSprite example = new CCSprite("example");
+            make_sprite("coin", 500, 100);
+            make_sprite("alien", 800, 100);
+            make_sprite("example", 1050, 100);
             CCSprite left = new CCSprite("rotateleft");
             CCSprite right = new CCSprite("rotateright");
-            CCLabel current_player = new CCLabel(currentplayer, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
-            CCLabel current_points= new CCLabel(currentpoints, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
-            CCLabel current_aliens = new CCLabel(currentaliens, "Fonts/Coalition", 36, CCLabelFormat.SpriteFont);
-
-            current_player.Position = new CCPoint(200, 100);
-            current_player.Color = label_color;
-            current_points.Position = new CCPoint(650, 100);
-            current_points.Color = label_color;
-            current_aliens.Position = new CCPoint(900, 100);
-            current_aliens.Color = label_color;
+            make_label(currentplayer, font, 36, 200, 100, label_color);
+            make_label(currentpoints, font, 36, 650, 100, label_color);
+            make_label(currentaliens, font, 36, 900, 100, label_color);
 
             overlay.AnchorPoint = new CCPoint(0, 0);
-            muntje.Position = new CCPoint(500, 100);
-            alien.Position = new CCPoint(800, 100);
-            example.Position = new CCPoint(1050, 100);
             left.Position = new CCPoint(1200, 100);
             right.Position = new CCPoint(1300, 100);
 
             AddChild(overlay);
-            AddChild(muntje);
-            AddChild(alien);
-            AddChild(example);
             AddChild(left);
             AddChild(right);
-            AddChild(current_player);
-            AddChild(current_points);
-            AddChild(current_aliens);
+        }
+        public void make_sprite(string name, int x, int y)
+        {
+            CCSprite sprite = new CCSprite(name);
+            sprite.Position = new CCPoint(x,y);
+            AddChild(sprite);
+        }
+        public void make_label(string text, string font, int textsize, int x, int y, CCColor3B color)
+        {
+            CCLabel label = new CCLabel( text, font, textsize,CCLabelFormat.SpriteFont);
+            label.Position = new CCPoint(x, y);
+            label.Color = color;
+            AddChild(label);
+
         }
         public void change_player()
         {
@@ -60,7 +59,7 @@ namespace ruigeruben
 
         public override void OnClick(CCPoint Location)
         {
-           
+            
         }
     }
 }
