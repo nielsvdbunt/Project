6decb5991ed46004caf57d9a62d99a35703df5d8
gotvet <gotvet@DESKTOP-BEUJ7OC>
2017-01-17 12:31:44 +0100

    overlay bijna klaar

--- ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
+++ ruigeruben/ruigeruben/Game/Drawing/Overlay.cs
@@ -32,32 +32,6 @@ namespace ruigeruben
             //rotateleft.OnClicked+=
             //alien_button.OnClicked+=
             //next.OnClicked+=
-            update_interface();
-        }
-        public void make_sprite(string name, int x, int y)//this method is for creating sprites
-        {
-            CCSprite sprite = new CCSprite(name);
-            sprite.Position = new CCPoint(x,y);
-            AddChild(sprite);
-        }
-        public void make_label(string text, string font, int textsize, int x, int y, CCColor3B color)//method for creating simple labels
-        {
-            CCLabel label = new CCLabel( text, font, textsize,CCLabelFormat.SpriteFont);
-            label.Position = new CCPoint(x, y);
-            label.Color = color;
-            AddChild(label);
-        }
-        public void update_interface()// in this method the labels and button are made that need to update everytime a player has his turn
-        {
-            string currentplayer="error";
-            string currentpoints = "error";
-            string currentaliens="error";
-            CCColor3B currentcolor = CCColor3B.White;
-            string amountoftiles = "123";
-            CCColor3B label_color = CCColor3B.White;
-
-            //PlayMenu playmenu = new PlayMenu();
-            //List<Player> playerlist = playmenu.m_Players;
 
             List<Player> playerlist = new List<Player>();
 
@@ -66,13 +40,13 @@ namespace ruigeruben
             Player player3 = new Player();
 
             player1.Name = "gotvet";
-            player1.Turn = false;
+            player1.Turn = true;
             player1.Points = 30;
             player1.NumberOfAliens = 7;
             player1.PlayerColor = CCColor3B.Red;
 
-            player2.Name = "Ruige Ruben";
-            player2.Turn = true;
+            player2.Name = "Ruben";
+            player2.Turn = false;
             player2.Points = -20;
             player2.NumberOfAliens = 4;
             player2.PlayerColor = CCColor3B.Blue;
@@ -86,6 +60,18 @@ namespace ruigeruben
             playerlist.Add(player1);
             playerlist.Add(player2);
             playerlist.Add(player3);
+
+            int numberoftiles = 123;
+
+            update_interface(playerlist, numberoftiles);
+        }
+        public void update_interface(List<Player> playerlist, int amountoftiles)// in this method the labels and button are made that need to update everytime a player has his turn
+        {
+            string currentplayer="error";
+            string currentpoints = "error";
+            string currentaliens="error";
+            CCColor3B currentcolor = CCColor3B.White;
+            CCColor3B label_color = CCColor3B.White;
 
             int t;
             for (t = 0; t < playerlist.Count; t++)
@@ -104,7 +90,7 @@ namespace ruigeruben
             make_label(currentplayer, font, 36, 200, 100, currentcolor);
             make_label(currentpoints, font, 36, 650, 100, label_color);
             make_label(currentaliens, font, 36, 900, 100, label_color);
-            make_label(amountoftiles, font, 36, 1850, 240, label_color);
+            make_label(amountoftiles.ToString(), font, 36, 1850, 240, label_color);
 
             for (int z=0; z<(playerlist.Count-1);z++)
             {
@@ -119,11 +105,32 @@ namespace ruigeruben
             //example.OnClicked+=
 
         }
+        public void make_sprite(string name, int x, int y)//this method is for creating sprites
+        {
+            CCSprite sprite = new CCSprite(name);
+            sprite.Position = new CCPoint(x, y);
+            AddChild(sprite);
+        }
+        public void make_label(string text, string font, int textsize, int x, int y, CCColor3B color)//method for creating simple labels
+        {
+            CCLabel label = new CCLabel(text, font, textsize, CCLabelFormat.SpriteFont);
+            label.Position = new CCPoint(x, y);
+            label.Color = color;
+            AddChild(label);
+        }
         public void make_playerlabel(string name, string points, string aliens, CCColor3B color)
         {
-            make_label(name, font, 20, 1800, 1000-r*200, color);
-            make_label(points, font, 20, 1750, 900-r*200, color);
-            make_label(aliens, font, 20, 1800, 900-r*200, color);
+            make_label(name, font, 20, 1800, 1050-r*100, color);
+            make_label(points, font, 20, 1800, 1000-r*100, color);
+            make_label(aliens, font, 20, 1900, 1000-r*100, color);
+            CCSprite smallalien = new CCSprite("alien");
+            CCSprite smallcoin = new CCSprite("coin");
+            smallalien.Scale = 0.25f;
+            smallcoin.Scale = 0.25f;
+            smallalien.Position = new CCPoint(1750,1000-r*100);
+            smallcoin.Position = new CCPoint(1860, 1000-r*100);
+            AddChild(smallalien);
+            AddChild(smallcoin);
             r++;
         }
         public override void OnBack()
