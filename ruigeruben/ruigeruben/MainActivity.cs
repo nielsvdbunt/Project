2a6a2c13fd1efb650daf34e11a491aabdc526a09
Ruub <rar.1993@hotmail.com>
2017-01-17 14:21:41 +0100

    tes

--- ruigeruben/ruigeruben/MainActivity.cs
+++ ruigeruben/ruigeruben/MainActivity.cs
@@ -92,8 +92,8 @@ namespace ruigeruben
                 GameScene gs = new GameScene(m_GameView, (InputGameInfo) o);       
                 m_GameView.Director.ReplaceScene(gs);
 
-                Task.Run(() => gs.StartGame());
-
+                // Task.Run(() => gs.StartGame());
+                gs.StartGame();
                 return;
             }
            
