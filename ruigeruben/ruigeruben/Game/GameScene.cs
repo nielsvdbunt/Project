ceb577ad0617051f344959f144b1767e6cf93462
Ruub <rar.1993@hotmail.com>
2017-01-17 14:06:40 +0100

    beweeg

--- ruigeruben/ruigeruben/Game/GameScene.cs
+++ ruigeruben/ruigeruben/Game/GameScene.cs
@@ -80,7 +80,7 @@ namespace ruigeruben
                     float x = touches[0].LocationOnScreen.X;
                     float y = touches[0].LocationOnScreen.Y;
                     CCPoint location = new CCPoint((x - 500),(-y + 700));
-                    
+                    m_BoardLayer.Position = location;
                 }
             }
         }
